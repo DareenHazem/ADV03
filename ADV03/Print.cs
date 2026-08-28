76c6d4bd68d81676bf2716efe027d535a3374a3d
DareenHazem <dareen.hazem2005@gmail.com>
2026-08-28 22:02:35 +0300

    Exercise 1: Student Grade Manager

---
+++ ADV03/Print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADV03
+{
+    internal class Print
+    {
+        public static void PrintList<T>(String Label,List<T> list)
+        {
+            if(list is not null && list.Count > 0 && Label is not null)
+            {
+                Console.Write($"{Label}: "); 
+                foreach (T item in list)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- ADV03/Program.cs
+++ ADV03/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ADV03
+﻿using System.Collections;
+
+namespace ADV03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Exercise 1: Student Grade Manager
+            ////1-
+            //List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
+            ////2-
+            //Print.PrintList("List",grades);
+            //Console.WriteLine($"Grades Count: {grades.Count}");
+            //Console.WriteLine($"First Grade: {grades[0]}, Last Grade: {grades[^1]}");
+            ////3-
+            //grades.Sort();
+            //Print.PrintList("Sorted List", grades);
+            ////4 
+            //Console.WriteLine($"First grade above 90: {grades.Find(x => x > 90)}");
+            ////5
+            //List<int> failingGrades = grades.FindAll(x => x <75);
+            //Print.PrintList("Grades below 75",failingGrades);
+            ////6
+            //grades.RemoveAll(x => x < 75);
+            //Print.PrintList("Grades below 75 after being removed", grades);
+            ////7
+            //Console.WriteLine($"Is any grade equals 100? {grades.Contains(100)}");
+            ////8
+            //List<string> gradeStrings = grades.ConvertAll(n=>$"Grade: {n}");
+            //Print.PrintList("List of String", gradeStrings);
+            #endregion
         }
     }
 }
